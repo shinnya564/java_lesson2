e53f7633a64fd69fc9e26cad75bbc5dcd3228b74
荒木　信哉 <shinnya@MacBook-Pro.local>
2020-11-12 17:39:38 +0900

    [finish]

---
+++ sample2-1/src/Calc1.java
@@ -0,0 +1,9 @@
+
+public class Calc1 {
+	public static void main(String[] args) {
+		System.out.println("加算の結果は" + (3+2) + "です");
+		System.out.println("減算の結果は" + (3-2) + "です");
+		System.out.println("乗算の結果は" + (3*2) + "です");
+		System.out.println("除算の結果は" + (3/2) + "です");
+	}
+}
---
+++ sample2-3/src/Calc3.java
@@ -0,0 +1,33 @@
+
+public class Calc3 {
+	public static void main(String[] args) {
+		System.out.println("問2-3");
+		int i = 0;
+		System.out.println(i + "*" + i + "=" + i * i);
+		i +=  1;
+		System.out.println(i + "*" + i + "=" + i * i);
+		i +=  1;
+		System.out.println(i + "*" + i + "=" + i * i);
+		i +=  1;
+		System.out.println(i + "*" + i + "=" + i * i);
+		i +=  1;
+		System.out.println(i + "*" + i + "=" + i * i);
+		i +=  1;
+		System.out.println(i + "*" + i + "=" + i * i);
+		i +=  1;
+		System.out.println(i + "*" + i + "=" + i * i);
+		i +=  1;
+		System.out.println(i + "*" + i + "=" + i * i);
+		i +=  1;
+		System.out.println(i + "*" + i + "=" + i * i);
+		i +=  1;
+		System.out.println(i + "*" + i + "=" + i * i);
+		i +=  1;
+		System.out.println(i + "*" + i + "=" + i * i);
+		i +=  1;
+		System.out.println("問2-4");
+		System.out.println("面積は" + (2+3) * 4 / 2);
+		System.out.println("問2-5");
+		System.out.println( 100000L * 100000L);
+	}
+}
